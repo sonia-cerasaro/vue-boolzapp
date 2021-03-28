87739d26bbcf3f5934bf6a788ffe0d5c65dabf21
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-28 16:51:26 +0200

    Milestone 1 completed

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,94 @@ var vue = new Vue(
   {
     el: '#root',
     data: {
-    messaggioInInput: '',
-  }
-});
+      contacts: [
+      	{
+          image: 'avatar_6.jpg',
+      		name: 'Michelle',
+      		avatar: '_1',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Hai portato a spasso il cane?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Ricordati di dargli da mangiare',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 16:15:22',
+      				text: 'Tutto fatto!',
+      				status: 'received'
+      			}
+      		],
+      	},
+      	{
+          image: 'avatar_4.jpg',
+      		name: 'George',
+      		avatar: '_2',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '20/03/2020 16:30:00',
+      				text: 'Ciao come stai?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '20/03/2020 16:30:55',
+      				text: 'Bene grazie! Stasera ci vediamo?',
+      				status: 'received'
+      			},
+      			{
+      				date: '20/03/2020 16:35:00',
+      				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+      				status: 'sent'
+      			}
+      		],
+      	},
+      	{
+          image: 'avatar_8.jpg',
+      		name: 'Kevin',
+      		avatar: '_3',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '28/03/2020 10:10:40',
+      				text: 'La Marianna va in campagna',
+      				status: 'received'
+      			},
+      			{
+      				date: '28/03/2020 10:20:10',
+      				text: 'Sicuro di non aver sbagliato chat?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '28/03/2020 16:15:22',
+      				text: 'Ah scusa!',
+      				status: 'received'
+      			}
+      		],
+      	},
+      	{
+          image: 'avatar_2.jpg',
+      		name: 'Luke',
+      		avatar: '_4',
+      		visible: true,
+      		messages: [
+      			{
+      				date: '10/01/2020 15:30:55',
+      				text: 'Lo sai che ha aperto una nuova pizzeria?',
+      				status: 'sent'
+      			},
+      			{
+      				date: '10/01/2020 15:50:00',
+      				text: 'Si, ma preferirei andare al cinema',
+      				status: 'received'
+      			}
+      		],
+      	},
+      ]
+    }
+  });
